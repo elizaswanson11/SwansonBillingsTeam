efc7cfacbe2438a7afb442a376999e13080e09d3
eliza <eliza@10.38.165.91>
2016-10-08 14:11:49 -0600

    10/8/2016 I added the OtherTribute Class

---
+++ HungerGames/src/byui/cit260/hungerGames/model/OtherTribute.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hungerGames.model;
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author eliza
+ */
+public class OtherTribute implements Serializable{
+    private String description;
+
+    public OtherTribute() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 13 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final OtherTribute other = (OtherTribute) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "OtherTribute{" + "description=" + description + '}';
+    }
+    
+    
+}
--- HungerGames/src/hungergames/HungerGames.java
+++ HungerGames/src/hungergames/HungerGames.java
@@ -14,6 +14,7 @@ import byui.cit260.hungerGames.model.Resource;
 import byui.cit260.hungerGames.model.Map;
 import byui.cit260.hungerGames.model.Player;
 import byui.cit260.hungerGames.model.Bag;
+import byui.cit260.hungerGames.model.OtherTribute;
 
 /**
  *
@@ -103,6 +104,13 @@ public class HungerGames {
         bagOne.setResourceCount(25);
         String bagInfo = bagOne.toString();
         System.out.println(bagInfo);
+        
+        
+        OtherTribute otherTributeOne = new OtherTribute();
+        
+        otherTributeOne.setDescription("This is a description of another tribute");
+        String otherTributeInfo = otherTributeOne.toString();
+        System.out.println(otherTributeInfo);
     }
    
     //Here is a change   
