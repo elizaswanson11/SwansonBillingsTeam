a65db9ffe493960c9e4f44e5b65f6b16b93786dc
Eliza Swanson <elizaswanson11@gmail.com>
2016-10-19 13:49:48 -0600

    10/07/16

---
+++ HungerGames/test/byui/cit260/hungerGames/control/ResourceControlTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hungerGames.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author elizaswanson
+ */
+public class ResourceControlTest {
+    
+    public ResourceControlTest() {
+    }
+
+    /**
+     * Test of calcResourceReward method, of class ResourceControl.
+     */
+    @Test
+    public void testCalcResourceReward() {
+        System.out.println("calcResourceReward");
+        
+        System.out.println("Test Case 1");
+        double capitolPopularity = 6;
+        ResourceControl instance = new ResourceControl();
+        double minResult = 1.5;
+        double maxResult = 15;
+        double result = instance.calcResourceReward(capitolPopularity);
+        assertTrue(result >= minResult && result <= maxResult);
+        
+        System.out.println("Test Case 2");
+        capitolPopularity = -3;
+        instance = new ResourceControl();
+        double expectedResult = -1;
+        result = instance.calcResourceReward(capitolPopularity);
+        assertEquals(expectedResult, result, 0.0);
+        
+    }
+    
+}
