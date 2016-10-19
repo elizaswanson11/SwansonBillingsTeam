e04221701a811b52fddd1e284941a4c422206c4e
Eliza Swanson <elizaswanson11@gmail.com>
2016-10-19 13:50:27 -0600

    10/07/16

--- HungerGames/src/byui/cit260/hungerGames/control/PuzzleCalculation.java
+++ HungerGames/src/byui/cit260/hungerGames/control/PuzzleCalculation.java
@@ -10,5 +10,5 @@ package byui.cit260.hungerGames.control;
  * @author elizaswanson
  */
 public class PuzzleCalculation {
-    
+
 }
--- HungerGames/src/byui/cit260/hungerGames/control/ResourceControl.java
+++ HungerGames/src/byui/cit260/hungerGames/control/ResourceControl.java
@@ -10,5 +10,22 @@ package byui.cit260.hungerGames.control;
  * @author elizaswanson
  */
 public class ResourceControl {
-    
+    public double calcResourceReward(double capitolPopularity) {
+        //calcResourceReward(capitolPopularity) {
+        //BEGIN
+        //IF (capitolPopularity < 3 OR capitolPopularity > 15) THEN
+        //RETURN -1
+        //award = MathRand(1, 10)
+        //resourceReward = award + (award * (capitolPopularity / 10))
+        //RETURN resourceReward
+        //}
+        if (capitolPopularity < 3 || capitolPopularity > 15) {
+            return -1;
+        }
+        else {
+            int award = (int)(Math.random() * 10 + 1);
+            double resourceReward = award + (award * (capitolPopularity / 10));
+            return resourceReward;
+        }
+    }
 }
