c317625a1f722bd489044f037a481fd23395050f
eliza <eliza@10.38.165.91>
2016-10-21 13:03:16 -0600

    10/21/2016

--- HungerGames/src/byui/cit260/hungerGames/control/PuzzleCalculation.java
+++ HungerGames/src/byui/cit260/hungerGames/control/PuzzleCalculation.java
@@ -10,5 +10,34 @@ package byui.cit260.hungerGames.control;
  * @author elizaswanson
  */
 public class PuzzleCalculation {
-
+    public double openRebellionBox(double capitolPopularity, double rebellionPopularity) {
+    //double openRebellionBox(double capitolPopularity, double rebellionPopularity) {
+    //BEGIN
+    //IF (capitolPopularity < 0 OR capitolPopularity > 15) THEN
+    //  RETURN -1
+    //IF (rebellionPopularity < 1 OR rebellionPopularity > 15) THEN
+    //  RETURN -1
+    //differenceScore = (rebellionPopularity – capitolPopularity) * 100
+    //IF(differenceScore > 100) THEN
+    //  RETURN 1
+    //ELSE
+    //  RETURN 0
+    //  }
+    //}
+    
+        if (capitolPopularity < 0 || capitolPopularity > 15) {
+            return -1;
+        }
+        if (rebellionPopularity < 0 || rebellionPopularity > 15){
+            return -1;
+        }
+        
+        double differenceScore = (rebellionPopularity - capitolPopularity) * 100;
+        if (differenceScore > 100) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
 }
