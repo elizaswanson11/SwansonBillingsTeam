cdb96b6b4c5358fa6382f2dcb363aa52af8c326a
eliza <eliza@10.38.165.91>
2016-10-25 13:41:12 -0600

    10/25/2016

---
+++ HungerGames/src/citbyui/cit260/hungerGames/view/StartProgramView.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.hungerGames.view;
+
+/**
+ *
+ * @author eliza
+ */
+public class StartProgramView {
+    
+    private String promptMessage;
+    
+    public StartProgramView() {
+        
+        this.promptMessage = "\nPlease enter your name: ";
+        
+        this.displayBanner();
+    }
+
+    public void displayBanner() {
+        System.out.println(
+        "/n**************************************************" +
+        "/n                                                  " +
+        "/n                 The Hunger Games.                " +
+        "/n This is a game of survival, not only physically  " +
+        "/n but socially as well. Your success in the Games  " +
+        "/n will be dependent on loyalty to our people, the  " +
+        "/n Rebel Alliance. What decisions will you make and " +
+        "/n where will these decisions take you? Let's begin " +
+        "/n and find out." +
+        "/n**************************************************"
+        );
+    }
+
+    public void displayStartProgramView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
--- HungerGames/src/hungergames/HungerGames.java
+++ HungerGames/src/hungergames/HungerGames.java
@@ -17,6 +17,7 @@ import byui.cit260.hungerGames.model.Bag;
 import byui.cit260.hungerGames.model.OtherTribute;
 import byui.cit260.hungerGames.model.FightTypeScene;
 import byui.cit260.hungerGames.model.ResourceTypeScene;
+import citbyui.cit260.hungerGames.view.StartProgramView;
 
 /**
  *
@@ -28,106 +29,8 @@ public class HungerGames {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Game gameOne = new Game();
-        
-        gameOne.setNumberPeople(24);
-        gameOne.setNumberOfTributes(4);
-        String gameInfo = gameOne.toString();
-        System.out.println(gameInfo);
-        
-
-        Location locationOne = new Location();
-        
-        locationOne.setRow(2);
-        locationOne.setColumn(4);
-        locationOne.setVisited(9);
-        locationOne.setAmountRemaining(24);
-        String locationInfo = locationOne.toString();
-        System.out.print(locationInfo);
-
-        
-        MainCharacter mainCharacterOne = new MainCharacter();
-        
-        mainCharacterOne.setRebelPopularity(13);
-        mainCharacterOne.setCapitolPopularity(5);
-        mainCharacterOne.setHealthLevel(12);
-        mainCharacterOne.setMaxRebelPopularity(20);
-        mainCharacterOne.setCapitolPopularity(20);
-        mainCharacterOne.setMaxHealthLevel(20);
-        String mainCharacterInfo = mainCharacterOne.toString();
-        System.out.println(mainCharacterInfo);
-        
-        
-        Character characterOne = new Character();
-        
-        characterOne.setName("Tommy");
-        characterOne.setCoordinantes(8);
-        characterOne.setSkillLevel(5.4);
-        String characterInfo = characterOne.toString();
-        System.out.println(characterInfo);
-        
-        
-        Scene sceneOne = new Scene();
-        
-        sceneOne.setDescription("This is the forest.");
-        sceneOne.setTravelHealth(1.2);
-        sceneOne.setDisplaySymbol("T");
-        String sceneInfo = sceneOne.toString();
-        System.out.println(sceneInfo);
-
-        
-        Resource resourceOne = new Resource();
-        
-        resourceOne.setDescription("This is a brief description.");
-        resourceOne.setHealthValue(13);
-        resourceOne.setMaxAmount(20);
-        String resourceInfo = resourceOne.toString();
-        System.out.println(resourceInfo);
-        
-        
-        Map mapOne = new Map();
-        
-        mapOne.setColumnCount(2);
-        mapOne.setRowCount(2);
-        String mapInfo = mapOne.toString();
-        System.out.println(mapInfo);
-        
-        
-        Player playerOne = new Player();
-        
-        playerOne.setName("Susan");
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        
-        Bag bagOne = new Bag();
-        
-        bagOne.setMaxResourceCount(15);
-        bagOne.setResourceCount(25);
-        String bagInfo = bagOne.toString();
-        System.out.println(bagInfo);
-        
-        FightTypeScene fightTypeSceneOne = new FightTypeScene();
-        
-        fightTypeSceneOne.setFightType(2);
-        String fightTypeSceneInfo = fightTypeSceneOne.toString();
-        System.out.println(fightTypeSceneInfo);
-        
-        ResourceTypeScene ResourceTypeSceneOne = new ResourceTypeScene();
-        
-        ResourceTypeSceneOne.setResourceType(1);
-        String ResourceTypeSceneInfo = ResourceTypeSceneOne.toString();
-        System.out.println(ResourceTypeSceneInfo);
-        
-        
-        
-        
-        
-        OtherTribute otherTributeOne = new OtherTribute();
-        
-        otherTributeOne.setDescription("This is a description of another tribute");
-        String otherTributeInfo = otherTributeOne.toString();
-        System.out.println(otherTributeInfo);
+       StartProgramView startProgramView = new StartProgramView();
+       startProgramView.displayStartProgramView();
     }
    
     //Here is a change   
