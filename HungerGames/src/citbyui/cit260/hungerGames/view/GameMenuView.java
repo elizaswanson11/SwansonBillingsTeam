2390fc16f368333d468a8b59daeee590de487cb8
eliza <eliza@10.38.165.91>
2016-10-28 13:54:35 -0600

    10/28/2016

---
+++ HungerGames/src/citbyui/cit260/hungerGames/view/GameMenuView.java
@@ -0,0 +1,19 @@
+package citbyui.cit260.hungerGames.view;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author eliza
+ */
+public class GameMenuView {
+
+    void displayMenu() {
+        System.out.println("\n*** displayMenu stub function called ***");
+    }
+    
+}
--- HungerGames/src/citbyui/cit260/hungerGames/view/MainMenuView.java
+++ HungerGames/src/citbyui/cit260/hungerGames/view/MainMenuView.java
@@ -5,6 +5,9 @@
  */
 package citbyui.cit260.hungerGames.view;
 
+import java.util.Scanner;
+import byui.cit260.hungerGames.control.GameControl;
+import hungergames.HungerGames;
 /**
  *
  * @author eliza
@@ -15,7 +18,14 @@ public class MainMenuView {
     private String menu;
 
     void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called ***");
+        boolean done = false;
+        do {
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q"))
+                return;
+            
+            done = this.doAction(menuOption);
+        } while (!done);
     }
 
     public MainMenuView() {
@@ -30,6 +40,70 @@ public class MainMenuView {
               + "\nQ - Quit"
               + "\n--------------------------------";
     }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
     
+            while (!valid) {
+                System.out.println("\n" + this.menu);
+        
+                value = keyboard.nextLine();
+                value = value.trim();
+        
+            if (value.length() < 1) {
+                System.out.println("\nInvalid value: value cannot be blank");
+                continue;
+            }
+        
+            break;
+        }
+    
+        return value;
+    }
+    
+    private boolean doAction(String value) {
+        value = value.toUpperCase();
+        
+        switch (value) {
+            case "B":
+                this.startNewGame();
+                break;
+            case "S":
+                this.saveGame();
+                break;
+            case "O":
+                this.startExistingGame();
+                break;
+            case "H":
+                this.displayHelpMenu();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try Again");
+                break;
+        }
+        
+        return false;
+    }   
+
+    private void startNewGame() {
+        GameControl.createNewGame(HungerGames.getPlayer());
+        
+        GameMenuView gameMenu = new GameMenuView();
+        gameMenu.displayMenu();
+    }
+
+    private void saveGame() {
+        System.out.println("*** saveGame function called ***");
+    }
+
+    private void startExistingGame() {
+        System.out.println("*** startExistingGame function called ***");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("*** displayHelpMenu function called ***");
+    }
 }
 
